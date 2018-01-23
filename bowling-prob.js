1bd1b2e9029624ff4aaf66d6fedff77887f4cc61
maheshhp <hpmahesh73@gmail.com>
2018-01-23 12:08:49 +0530

    Passed existing tests

--- bowling-prob.js
+++ bowling-prob.js
@@ -1,11 +1,56 @@
 function calculateSum(scoreArray) {
-
+  let totalScore = 0;
+  let i = 0;
+  for (i = 0; i < scoreArray.length - 1; i += 1) {
+    if (scoreArray[i].length === 1) {
+      if (scoreArray[i + 1].length === 1) {
+        totalScore += scoreArray[i][0] + scoreArray[i + 1][0] + scoreArray[i + 2][0];
+      } else {
+        totalScore += scoreArray[i][0] + scoreArray[i + 1][0] + scoreArray[i + 1][1];
+      }
+    } else if ((scoreArray[i][0] + scoreArray[i][1]) === 10) {
+      totalScore += scoreArray[i][0] + scoreArray[i][1] + scoreArray[i + 1][0];
+    } else if (((scoreArray[i][0] + scoreArray[i][1]) >= 10)
+     || scoreArray[i][0] < 0 || scoreArray[i][1] < 0) {
+      return 'Invalid input';
+    } else {
+      totalScore += scoreArray[i][0] + scoreArray[i][1];
+    }
+  }
+  for (let j = 0; j < scoreArray[i].length; j += 1) {
+    totalScore += scoreArray[i][j];
+  }
+  return totalScore;
 }
 function calculateScore(rollArray) {
-
+  let frameCount = 0;
+  let scoreArray = [];
+  let i = 0;
+  for (i = 0; i < rollArray.length && frameCount < 9; i += 1) {
+    if (rollArray[i] === 10) {
+      scoreArray[frameCount] = [10];
+      frameCount += 1;
+    } else {
+      scoreArray[frameCount] = [rollArray[i], rollArray[i + 1]];
+      i += 1;
+      frameCount += 1;
+    }
+  }
+  if ((rollArray.length - i) === 3) {
+    scoreArray[frameCount] = [rollArray[i], rollArray[i + 1], rollArray[i + 2]];
+  } else {
+    scoreArray[frameCount] = [rollArray[i], rollArray[i + 1]];
+  }
+  return calculateSum(scoreArray);
 }
 function roll(bowlingFrames) {
-
+  if (bowlingFrames.length < 10) {
+    return 'Invalid input';
+  }
+  return calculateScore(bowlingFrames);
 }
 
-module.exports = roll;
+module.exports = {
+  roll,
+  calculateSum,
+};
--- bowling-prob.test.js
+++ bowling-prob.test.js
@@ -1,17 +1,26 @@
-const roll = require('./bowling-prob');
+const exported = require('./bowling-prob');
+
+const { calculateSum } = exported;
+const { roll } = exported;
+
 
 describe('Strike in 10th frame', () => {
   test('Two fill balls have been given', () => {
     expect(roll([10, 3, 6, 3, 6, 4, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 5, 3])).toBe(113);
   });
   test('Expected score from calculation returned for non-strike in fill balls', () => {
-    expect(roll([10, 3, 6, 3, 6, 4, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 10, 8, 10])).toBe(132);
+    expect(roll([10, 3, 6, 3, 6, 4, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 10, 8, 10])).toBe(133);
   });
   test('Expected score from calculation returned for strike in fill balls', () => {
     expect(roll([10, 3, 6, 3, 6, 4, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 10, 10, 10])).toBe(135);
   });
   test('Expected score from calculation returned for spare in fill balls', () => {
     expect(roll([10, 3, 6, 3, 6, 4, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 5, 5, 3])).toBe(118);
+  });
+});
+describe('Tests for calculateSum helper function', () => {
+  test('Expected score from helper function', () => {
+    expect(calculateSum([[10], [3, 6], [3, 4], [1, 8]])).toBe(44);
   });
 });
 describe('Spare in 10th frame', () => {
@@ -37,5 +46,8 @@ describe('Regular game', () => {
   });
   test('Invalid input string returned for invalid array input containing > 10 in regular frame', () => {
     expect(roll([10, 3, 6, 3, 6, 5, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 10, 10, 10])).toMatch('Invalid input');
+  });
+  test('Invalid input string returned for invalid array input containing < 0 in regular frame', () => {
+    expect(roll([10, 3, 6, 3, 6, 5, 6, 3, 6, 10, 3, 6, 3, 6, 3, -1, 10, 10, 10])).toMatch('Invalid input');
   });
 });
