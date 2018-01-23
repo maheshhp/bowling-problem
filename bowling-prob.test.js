53ccf6ad50bf310d23d5485e6fa9df507c326824
maheshhp <hpmahesh73@gmail.com>
2018-01-23 10:32:55 +0530

    Added tests for fill ball with strike for the bowling game problem

---
+++ bowling-prob.test.js
@@ -0,0 +1,14 @@
+describe('Strike in 10th frame', () => {
+  test('Two fill balls have been given', () => {
+    expect().toBe();
+  });
+  test('Expected score from calculation returned for non-strike in fill balls', () => {
+    expect().toBe();
+  });
+  test('Expected score from calculation returned for strike in fill balls', () => {
+    expect().toBe();
+  });
+  test('Expected score from calculation returned for spare in fill balls', () => {
+    expect().toBe();
+  });
+});
